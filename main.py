5575d4c7ae103d6d344c50fa2cf94c87e191506b
Mihil Jose <>
2021-04-14 06:00:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+age = input("What is your current age?")
+
+remaining_years = 90-int(age)
+
+days = remaining_years*365
+weeks = remaining_years * 52
+months = remaining_years * 12
+
+print(f"you have {days} days. {weeks} weeks, and {months} months left")
+
+
+
